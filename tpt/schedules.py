4696ef03fd40338f97c5457af9fcaabeda5c095c
Mihai Balint <mihai@cs.upt.ro>
2015-03-10 07:44:16 +0200

    show download & convert progress, skip downloading interval schedules and broken links

--- tpt/schedules.py
+++ tpt/schedules.py
@@ -6,20 +6,33 @@ import lxml.html
 import re
 import requests
 from subprocess import call
+import sys
 
+known_freq_pdfs = [
+    '33.pdf', '40.pdf']
 known_pdfs = [
-    '13a.pdf', '13b.pdf', '21a.pdf', '21b.pdf', '22a.pdf', '22b.pdf',
-    '28a.pdf', '28b.pdf', '29a.pdf', '29b.pdf', '32a.pdf', '32b.pdf',
-    '33.pdf', '33b-a.pdf', '33b-b.pdf', '40.pdf', '46a.pdf', '46b.pdf',
-    '8b-a.pdf', '8b-b.pdf', 'e1a.pdf', 'e1b.pdf', 'e2a.pdf', 'e2b.pdf',
-    'e3a.pdf', 'e3b.pdf', 'e4a.pdf', 'e4b.pdf', 'e4b-a.pdf', 'e4b-b.pdf',
-    'e6.pdf', 'e6a.pdf', 'e6b.pdf', 'e7a.pdf', 'e7b.pdf',
-    'e8a.pdf', 'e8b.pdf', 'm30.pdf', 'm35a.pdf', 'm35b.pdf',
-    'm43a.pdf', 'm43b.pdf', 'm44.pdf', 'm45a.pdf', 'm45b.pdf']
+    '13a.pdf', '13b.pdf', '21a.pdf', '21b.pdf',
+    '22a.pdf', '22b.pdf', '28a.pdf', '28b.pdf',
+    '29a.pdf', '29b.pdf', '32a.pdf', '32b.pdf',
+    '33b-a.pdf', '33b-b.pdf', '46a.pdf', '46b.pdf',
+    '8b-a.pdf', '8b-b.pdf', 'e1a.pdf', 'e1b.pdf',
+    'e2a.pdf', 'e2b.pdf', 'e3a.pdf', 'e3b.pdf',
+    'e4a.pdf', 'e4b.pdf', 'e4b-a.pdf', 'e4b-b.pdf',
+    'e6.pdf', 'e7a.pdf', 'e7b.pdf',
+    'e8a.pdf', 'e8b.pdf', 'm30.pdf',
+    'm35a.pdf', 'm35b.pdf', 'm43a.pdf', 'm43b.pdf',
+    'm44.pdf', 'm45a.pdf', 'm45b.pdf']
+
+known_broken_links = [
+    'e6a.pdf', 'e6b.pdf'
+]
 
 
 def download_known_pdfs():
+    sys.stdout.write("Downloading:")
     for pdf in known_pdfs:
+        sys.stdout.write(" {0}".format(pdf[:-4]))
+        sys.stdout.flush()
         r = requests.get('http://www.ratt.ro/grafice/{0}'.format(pdf),
                          stream=True)
         if not r.ok:
@@ -30,13 +43,22 @@ def download_known_pdfs():
                 if piece:
                     f.write(piece)
                     f.flush()
+    sys.stdout.write("\nDone.\n")
+    sys.stdout.flush()
 
 
 def convert_known_pdfs():
+    sys.stdout.write("Converting: ")
+    call(['rm', '-rf', 'txts'])
+    call(['mkdir', '-p', 'txts'])
     for pdf in known_pdfs:
+        sys.stdout.write(" {0}".format(pdf[:-4]))
+        sys.stdout.flush()
         call(['pdftotext', '-layout', '-q',
               'pdfs/{0}'.format(pdf),
               'txts/{0}.txt'.format(pdf)])
+    sys.stdout.write("\nDone.\n")
+    sys.stdout.flush()
 
 
 def parse_download_html(download_html):
@@ -49,29 +71,33 @@ def parse_download_html(download_html):
 def parse_grafice_html(grafice_html):
     matches = re.finditer('\'/grafice/.*\.pdf\'', grafice_html)
     results = [str(m.group(0)[10:-1]) for m in matches]
-    # import ipdb; ipdb.set_trace()
     return sorted(set(results))
 
 
 def compare_report(download_html, grafice_html):
     known = set(known_pdfs)
+    all_published = set(list(download_html) + list(grafice_html))
+    download_html = set(download_html) - set(known_broken_links)
+    download_html = set(download_html) - set(known_freq_pdfs)
+    grafice_html = set(grafice_html) - set(known_broken_links)
+    grafice_html = set(grafice_html) - set(known_freq_pdfs)
     print 'Comparing known_pdfs with download.html'
     print '\t    known_pdfs unique: {0}'.format(
-        ', '.join(known - set(download_html)))
+        ', '.join(known - download_html))
     print '\t download.html unique: {0}'.format(
-        ', '.join(set(download_html) - known))
+        ', '.join(download_html - known))
 
     print 'Comparing known_pdfs with grafice_XXX.html'
     print '\t    known_pdfs unique: {0}'.format(
-        ', '.join(known - set(grafice_html)))
+        ', '.join(known - grafice_html))
     print '\t grafice_.html unique: {0}'.format(
-        ', '.join(set(grafice_html) - known))
+        ', '.join(grafice_html - known))
 
     print 'Comparing grafice_XXX.html with download.html'
     print '\t grafice_.html unique: {0}'.format(
-        ', '.join(set(grafice_html) - set(download_html)))
+        ', '.join(grafice_html - download_html))
     print '\t download.html unique: {0}'.format(
-        ', '.join(set(download_html) - set(grafice_html)))
+        ', '.join(download_html - grafice_html))
 
     published = set(list(download_html) + list(grafice_html))
     print 'Comparing known_pdfs with published'
@@ -79,7 +105,8 @@ def compare_report(download_html, grafice_html):
         ', '.join(known - published))
     print '\t  published unique: {0}'.format(
         ', '.join(published - known))
-    return published != known
+    all_known = set(known_pdfs + known_freq_pdfs + known_broken_links)
+    return all_published != all_known
 
 
 def main():
